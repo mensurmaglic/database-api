2fd2440adf319a5f496ba56e9d0da92b65aa7104
Mensur Maglić <mensur.maglic@a1.at>
2025-04-11 09:32:29 +0200

    move read func to read

--- src/items/api.py
+++ src/items/api.py
@@ -18,6 +18,12 @@ def create_item(item: models.ItemCreate, db: Session = Depends(database.get_db))
     return db_item
 
 
+@router.get("/", response_model=list[models.ItemRead])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+    items = db.query(models.Item).offset(skip).limit(limit).all()
+    return items
+
+
 @router.get("/{item_id}", response_model=models.ItemRead)
 def read_item(item_id: int, db: Session = Depends(database.get_db)):
     db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
@@ -46,9 +52,3 @@ def delete_item(item_id: int, db: Session = Depends(database.get_db)):
     db.delete(db_item)
     db.commit()
     return db_item
-
-
-@router.get("/", response_model=list[models.ItemRead])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
-    items = db.query(models.Item).offset(skip).limit(limit).all()
-    return items
